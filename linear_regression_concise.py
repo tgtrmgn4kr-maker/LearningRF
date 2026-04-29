c3b70349092c487676a021a52da285a6e84207d3
OwenSong <tgtrmgn4kr@privaterelay.appleid.com>
2026-04-29 22:29:30 +0800

    Learning Softmax

---
+++ linear_regression_concise.py
@@ -0,0 +1,67 @@
+import os
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+import random as r
+
+import matplotlib.pyplot as plt
+import torch
+from torch import nn
+
+def synthesize_linear_data(w, b, num_examples):
+    """Generate y = Xw + b + noise."""
+    X = torch.normal(
+        0, 1, (num_examples, len(w))
+    )  # mu=0, sigma=1 size=(num_examples*len(w))
+    y = X @ w + b  # (1000, 2) @ (2,) + (1,) -> (1000, 1)
+    y += torch.normal(0, 0.01, y.shape)  # y.shape=(1000, 1) noise with mu=0, sigma=0.01
+    return X, y.reshape((-1, 1))
+
+
+def dataloader(features, labels, batch_size):
+    """A generator function to yield batches of features and labels."""
+    num_examples = len(features)
+    indices = list(range(num_examples))
+    r.shuffle(indices)  # shuffle the data
+    for i in range(0, num_examples, batch_size):
+        batch_indices = torch.tensor(indices[i : min(i + batch_size, num_examples)])
+        yield features[batch_indices], labels[batch_indices]
+
+def load_array(data_arrays, batch_size, is_train=True):
+    """Construct a PyTorch data iterator."""
+    dataset = torch.utils.data.TensorDataset(*data_arrays)
+    return torch.utils.data.DataLoader(dataset, batch_size, shuffle=is_train)
+
+
+true_w = torch.tensor([2, -3.4])
+true_b = 4.2
+features, labels = synthesize_linear_data(true_w, true_b, 1000)
+
+batch_size = 10
+data_iter = load_array((features, labels), batch_size)
+
+print(next(iter(data_iter))) # 一組 features 和 labels 的小批量数据
+
+net = nn.Sequential(nn.Linear(2, 1))
+'''
+Sequential是一個容器，將多個層組合在一起，按照順序執行。
+在這裡，我們使用了一個線性層nn.Linear(2, 1)，輸入特徵的維度是2，輸出特徵的維度是1。
+'''
+
+print(net[0].weight.data)  # 隨機初始化的權重
+print(net[0].bias.data)    # 隨機初始化的偏置
+
+loss = nn.MSELoss()  # 均方誤差損失函數
+trainer = torch.optim.SGD(net.parameters(), lr=0.03)  # 隨機梯度下降優化器，學習率為0.03
+
+num_epochs = 3
+for epoch in range(num_epochs):
+    for X, y in data_iter:
+        loss_value = loss(net(X), y)  # 計算損失
+        trainer.zero_grad()  # 清空梯度
+        loss_value.backward()  # 反向傳播計算梯度
+        trainer.step()  # 更新參數
+
+    with torch.no_grad():
+        train_l = loss(net(features), labels)
+        print(f"epoch {epoch + 1}, loss {train_l:f}")
--- softmax_regression.py
+++ softmax_regression.py
@@ -5,7 +5,6 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
-from torch import nn
 import torchvision
 from torch.utils import data
 from torchvision import transforms
@@ -44,7 +43,7 @@ def show_images(
         axes = [ax for row in axes for ax in row]  # 如果 axes 是嵌套列表，则扁平化
     else:
         axes = [axes]  # 如果 axes 是单个 Axes 对象，则放入列表中
-    for i, (ax, img) in enumerate(zip(axes, imgs)):
+    for i, (ax, img) in enumerate(zip(axes, imgs)): # 使用 zip 函数同时迭代 axes 和 imgs，获取每个子图的 Axes 对象和对应的图像数据。
         if torch.is_tensor(img):
             # 图片张量
             ax.imshow(img.numpy())
@@ -95,6 +94,7 @@ def load_data_fashion_mnist(batch_size, resize=None):
         mnist_train,
     )
 
+
 class Accumulator:
     """在n个变量上累加。"""
 
@@ -110,28 +110,114 @@ class Accumulator:
     def __getitem__(self, idx):
         return self.data[idx]
 
+
+class Animator:
+    """在动画中绘制数据。"""
+
+    def __init__(
+        self,
+        xlabel=None,
+        ylabel=None,
+        legend=None,
+        xlim=None,
+        ylim=None,
+        xscale="linear",
+        yscale="linear",
+        fmts=("-r", "m--", "g-.", "b:"),
+        nrows=1,
+        ncols=1,
+        figsize=(3.5, 2.5),
+    ):
+        # 增加了一个参数fmts，表示不同数据序列的线条格式，默认为("-r", "m--", "g-.", "b:")。
+        if legend is None:
+            legend = []
+        plt.rcParams["figure.figsize"] = figsize
+        self.fig, self.axes = plt.subplots(nrows, ncols, figsize=figsize)
+        if isinstance(self.axes, np.ndarray):
+            self.axes = self.axes.flatten()[0]
+        self.axes.set_xlabel(xlabel)
+        self.axes.set_ylabel(ylabel)
+        self.axes.set_xscale(xscale)
+        self.axes.set_yscale(yscale)
+        self.axes.set_xlim(xlim)
+        self.axes.set_ylim(ylim)
+        self.legend = legend
+        self.fmts = fmts
+
+        self.config_axes = (
+            lambda: self.axes.set_xlabel(xlabel)
+            or self.axes.set_ylabel(ylabel)
+            or self.axes.set_xscale(xscale)
+            or self.axes.set_yscale(yscale)
+            or self.axes.set_xlim(xlim)
+            or self.axes.set_ylim(ylim)
+        )
+
+        self.X, self.Y = None, None
+        plt.show(block=False)
+
+    def add(self, x, y):
+        # 在add方法中，我们使用了zip函数将x、y和fmts三个列表打包在一起，这样就可以同时迭代它们。
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if self.X is None:
+            self.X = [[] for _ in range(n)]
+        if self.Y is None:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+
+        self.axes.cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes.plot(x, y, fmt)
+        if self.legend:
+            self.axes.legend(self.legend)
+
+        self.config_axes()
+        self.fig.canvas.draw()
+        plt.pause(0.001)
+
+
 def softmax(X: torch.Tensor) -> torch.Tensor:
     X_exp = torch.exp(X)
     partition = X_exp.sum(1, keepdim=True)
     return X_exp / partition  # 这里应用了广播机制
 
+
 num_inputs = 784
 num_outputs = 10
 
+global w, b
 w = torch.normal(0, 0.01, size=(num_inputs, num_outputs), requires_grad=True)
 b = torch.zeros(num_outputs, requires_grad=True)
 
+
 def net(X: torch.Tensor) -> torch.Tensor:
+    """将输入X重塑为二维张量，第一维是样本数量，第二维是特征数量
+    ，然后与权重矩阵w进行矩阵乘法，并加上偏置b，最后通过softmax函数得到预测的概率分布"""
+
     return softmax(torch.matmul(X.reshape((-1, w.shape[0])), w) + b)
 
-def cross_entropy(y_hat: torch.Tensor, y: torch.Tensor) -> torch.Tensor: # 交叉熵损失函数
+
+def cross_entropy_loss(
+    y_hat: torch.Tensor, y: torch.Tensor
+) -> torch.Tensor:  # 交叉熵损失函数
     return -torch.log(y_hat[range(len(y_hat)), y])
 
-def accuracy(y_hat: torch.Tensor, y: torch.Tensor) -> float: # 计算预测正确的数量
-    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1: # y大於等於維度2，且第二维的大小大于1，说明y_hat是一个二维张量，且每行包含多个类别的预测概率。
+
+def accuracy(y_hat: torch.Tensor, y: torch.Tensor) -> float:  # 计算预测正确的数量
+    if (
+        len(y_hat.shape) > 1 and y_hat.shape[1] > 1
+    ):  # y大於等於維度2，且第二维的大小大于1，说明y_hat是一个二维张量，且每行包含多个类别的预测概率。
         y_hat = y_hat.argmax(dim=1)
     cmp = y_hat.type(y.dtype) == y
     return float(cmp.type(y.dtype).sum())
+
 
 def evaluate_accuracy(net, data_iter) -> float:
     if isinstance(net, torch.nn.Module):
@@ -139,10 +225,13 @@ def evaluate_accuracy(net, data_iter) -> float:
     metric = Accumulator(2)  # 正确预测数、预测总数
     with torch.no_grad():
         for X, y in data_iter:
-            metric.add(accuracy(net(X), y), y.numel())
+            metric.add(
+                accuracy(net(X), y), y.numel()
+            )  # 计算当前批次的正确预测数和总预测数，并将它们累加到metric中。
     return metric[0] / metric[1]
 
-def train_epoch_ch3(net, train_iter, loss, updater):
+
+def train_epoch(net, train_iter, loss, updater):
     """训练模型一个迭代周期"""
     if isinstance(net, torch.nn.Module):
         net.train()  # 将模型设置为训练模式
@@ -157,13 +246,33 @@ def train_epoch_ch3(net, train_iter, loss, updater):
         else:
             loss_value.sum().backward()
             updater(X.shape[0])
-        metric.add(float(loss_value.sum()), accuracy(y_hat, y), y.numel())
+        metric.add(loss_value.sum().item(), accuracy(y_hat, y), y.numel())
     # 返回训练损失和训练精度
     return metric[0] / metric[2], metric[1] / metric[2]
 
+
+def train(net, train_iter, test_iter, loss, num_epochs, updater):
+    animator = Animator(
+        xlabel="epoch",
+        ylabel="loss/accuracy",
+        xlim=[1, num_epochs],
+        ylim=[0.3, 0.9],
+        legend=["train loss", "train acc", "test acc"],
+    )
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        animator.add(epoch + 1, train_metrics + (test_acc,))
+    train_loss, train_acc = train_metrics
+  #  assert train_loss < 0.5, f"训练损失过大: {train_loss}"
+
+  #  assert train_acc <= 1 and train_acc > 0.7, f"训练精度过低: {train_acc}"
+  #  assert test_acc <= 1 and test_acc > 0.7, f"测试精度过低: {test_acc}"
+
+
 def main():
     train_iter, _, mnist_test, mnist_train = load_data_fashion_mnist(
-        batch_size=18, resize=224
+        batch_size=18
     )  # 将图像调整为224x224像素，以适应后续的卷积神经网络输入要求。
 
     X, y = next(
@@ -171,9 +280,9 @@ def main():
     )  # 从训练数据迭代器中获取一个批次的数据，X是图像数据，y是对应的标签。
 
     show_images(
-        X.reshape(
-            18, 224, 224
-        ),  # 18张图像，每张图像是224x224像素的单通道图像，因此将X重塑为(18, 224, 224)的形状。
+        X.squeeze(
+            1
+        ),  # 去掉通道维度，得到形状为(18, 224, 224)的张量，每个图像是224x224像素的灰度图。
         2,  # 行数
         9,  # 列数
         titles=get_fashion_mnist_labels(y),
@@ -181,14 +290,14 @@ def main():
 
     plt.show()
 
-    print(len(mnist_train), len(mnist_test)) # 输出训练集和测试集的样本数量，分别为60000和10000。
+    print(
+        len(mnist_train), len(mnist_test)
+    )  # 输出训练集和测试集的样本数量，分别为60000和10000。
 
     for X, y in train_iter:
         print(X.shape, X.dtype, y.shape, y.dtype)
         print(f"{timer.stop():.2f} sec")
         break
-
-    # 下面的代码展示了如何使用softmax函数将模型的输出转换为概率分布。
 
     batch_size = 256
 
@@ -198,10 +307,18 @@ def main():
 
     print(
         X.sum(1, keepdim=True)
-    )  # 沿着维度1（列）求和，保持维度不变 tensor([[6.],[15.]])
+    )  # 沿着维度1（列）求和，保持维度不变 tensor([[6.],[15.]])))
     print(
         X.sum(0, keepdim=True)
-    )  # 沿着维度0（行）求和，保持维度不变 tensor([[5., 7., 9.]])
+    )  # 沿着维度0（行）求和，保持维度不变 tensor([[5., 7., 9.]]))
+
+    lr = 0.1
+    num_epochs = 10
+
+    updater = torch.optim.SGD([w, b], lr=lr)
+
+    train(net, train_iter, test_iter, cross_entropy_loss, num_epochs, updater)
+    plt.show()  # 确保图表最后被显示出来
 
 
 if __name__ == "__main__":
